54965efd2680dce85e7446369107e408486dc736
HungP <hpphann@gmail.com>
2019-09-30 04:36:11 -0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const animatedElements = document.getElementsByClassName("animate");
 
 for (var i = 0; i < animatedElements.length; i++) {
-    animatedElements[i].classList.add("fadeInFromTopToBottom")
-    if (i != animatedElements.length - 1) {
-        // var style = document.createElement('style');
-        // style.type = 'text/css';
-        // style.innerHTML = '.delay { opacity: 0; animation-delay: ${i+1}s; animation-fill-mode: forwards;}';
-        // animatedElements[i+1].className = "delay"
+    if (i % 2 == 0) {
+        animatedElements[i].classList.add("fadeInFromLeftToRight")
+    } else {
+        animatedElements[i].classList.add("fadeInFromRightToLeft")
     }
 }
