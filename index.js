c5d2ee4d4f23e8dc21be14efbd8bd1a2aa002f69
HungP <hpphann@gmail.com>
2019-10-01 01:32:44 -0700

    update

--- index.js
+++ index.js
@@ -8,16 +8,80 @@ for (var i = 0; i < animatedElements.length; i++) {
     }
 }
 
-console.log("initial screen width=" + screen.width)
-if (screen.width < 600) {
-    
+// change layout upon window's innerWidth
+console.log(window.innerWidth)
+if (window.innerWidth < 575) {
+    var container = document.getElementsByClassName("container");
+    var leftInnerContainer = document.getElementsByClassName("leftInnerContainer");
+    var rightInnerContainer = document.getElementsByClassName("rightInnerContainer");
+
+    for (var i = 0; i < container.length; i++) {
+        // add before remove
+        container[i].classList.add("containerMobile")
+        container[i].classList.remove("container")
+    }
+
+    for (var i = 0; i < leftInnerContainer.length; i++) {
+        leftInnerContainer[i].classList.add("leftInnerContainerMobile")
+        leftInnerContainer[i].classList.remove("leftInnerContainer")
+    }
+
+    for (var i = 0; i < rightInnerContainer.length; i++) {
+        rightInnerContainer[i].classList.add("rightInnerContainerMobile")
+        rightInnerContainer[i].classList.remove("rightInnerContainer")
+    }
 }
 
-var onresize = function(e) {
+// change layout upon resizing
+var onresize = function (e) {
     width = e.target.outerWidth;
     height = e.target.outerHeight;
 
     console.log("resizing width=" + width)
- }
- window.addEventListener("resize", onresize);
+    if (width < 575) {
+        var container = document.getElementsByClassName("container");
+        var leftInnerContainer = document.getElementsByClassName("leftInnerContainer");
+        var rightInnerContainer = document.getElementsByClassName("rightInnerContainer");
+
+        for (var i = 0; i < container.length; i++) {
+            // add before remove
+            container[i].classList.add("containerMobile")
+            container[i].classList.remove("container")
+        }
+
+        for (var i = 0; i < leftInnerContainer.length; i++) {
+            leftInnerContainer[i].classList.add("leftInnerContainerMobile")
+            leftInnerContainer[i].classList.remove("leftInnerContainer")
+        }
+
+        for (var i = 0; i < rightInnerContainer.length; i++) {
+            rightInnerContainer[i].classList.add("rightInnerContainerMobile")
+            rightInnerContainer[i].classList.remove("rightInnerContainer")
+        }
+
+
+    }
+    else if (width >= 575) {
+        container = document.getElementsByClassName("containerMobile");
+        leftInnerContainer = document.getElementsByClassName("leftInnerContainerMobile");
+        rightInnerContainer = document.getElementsByClassName("rightInnerContainerMobile");
+
+        for (var i = 0; i < container.length; i++) {
+            // add before remove
+            container[i].classList.add("container")
+            container[i].classList.remove("containerMobile")
+        }
+
+        for (var i = 0; i < leftInnerContainer.length; i++) {
+            leftInnerContainer[i].classList.add("leftInnerContainer")
+            leftInnerContainer[i].classList.remove("leftInnerContainerMobile")
+        }
+
+        for (var i = 0; i < rightInnerContainer.length; i++) {
+            rightInnerContainer[i].classList.add("rightInnerContainer")
+            rightInnerContainer[i].classList.remove("rightInnerContainerMobile")
+        }
+    }
+}
+window.addEventListener("resize", onresize);
 
