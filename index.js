12b1bde1cace6384a5b7653c53de45e7df52dba3
Hung Phan <17309881+hp0101@users.noreply.github.com>
2019-10-01 20:30:03 -0700

    update

--- index.js
+++ index.js
@@ -1,163 +1,213 @@
 const animatedElements = document.getElementsByClassName("animate");
 
 for (var i = 0; i < animatedElements.length; i++) {
-    if (i % 2 == 0) {
-        animatedElements[i].classList.add("fadeInFromLeftToRight")
-    } else {
-        animatedElements[i].classList.add("fadeInFromRightToLeft")
-    }
+  if (i % 2 == 0) {
+    animatedElements[i].classList.add("fadeInFromLeftToRight");
+  } else {
+    animatedElements[i].classList.add("fadeInFromRightToLeft");
+  }
 }
 
+// remove all animation
+setTimeout(function() {
+  for (var i = animatedElements.length - 1; i >= 0; i--) {
+    if (i % 2 == 0) {
+      animatedElements[i].classList.remove("fadeInFromLeftToRight");
+    } else {
+      animatedElements[i].classList.remove("fadeInFromRightToLeft");
+    }
+  }
+}, 3000);
+
 function addMobile(container, leftInnerContainer, rightInnerContainer, hr) {
-    // add and then remove
-    // add containerMobile
-    for (var i = 0; i < container.length; i++) {
-        container[i].classList.add("containerMobile")
-    }
+  // add and then remove
+  // add containerMobile
+  for (var i = 0; i < container.length; i++) {
+    container[i].classList.add("containerMobile");
+  }
 
-    // remove container - USE REVERSE LOOP WHEN REMOVING CLASS!!
-    for (var i = container.length - 1; i >= 0; i--) {
-        container[i].classList.remove("container")
-    }
+  // remove container - USE REVERSE LOOP WHEN REMOVING CLASS!!
+  for (var i = container.length - 1; i >= 0; i--) {
+    container[i].classList.remove("container");
+  }
 
-    // debugging - container.length is now empty
-    // for (var i = container.length-1; i >= 0; i--) {
-    //     console.log(container[i])
-    // }
+  // debugging - container.length is now empty
+  // for (var i = container.length-1; i >= 0; i--) {
+  //     console.log(container[i])
+  // }
 
-    // add hrWidth
-    for (var i = 0; i < hr.length; i++) {
-        hr[i].classList.add("hrWidth")
-    }
+  // add hrWidth
+  for (var i = 0; i < hr.length; i++) {
+    hr[i].classList.add("hrWidth");
+  }
 
-    // add leftInnerContainerMobile
-    for (var i = 0; i < leftInnerContainer.length; i++) {
-        leftInnerContainer[i].classList.add("leftInnerContainerMobile")
-    }
+  // add leftInnerContainerMobile
+  for (var i = 0; i < leftInnerContainer.length; i++) {
+    leftInnerContainer[i].classList.add("leftInnerContainerMobile");
+  }
 
-    // remove leftInnerContainer - USE REVERSE LOOP WHEN REMOVING CLASS!!
-    for (var i = leftInnerContainer.length - 1; i >= 0; i--) {
-        leftInnerContainer[i].classList.remove("leftInnerContainer")
-    }
+  // remove leftInnerContainer - USE REVERSE LOOP WHEN REMOVING CLASS!!
+  for (var i = leftInnerContainer.length - 1; i >= 0; i--) {
+    leftInnerContainer[i].classList.remove("leftInnerContainer");
+  }
 
-    // add rightInnerContainerMobile
-    for (var i = 0; i < rightInnerContainer.length; i++) {
-        rightInnerContainer[i].classList.add("rightInnerContainerMobile")
-    }
+  // add rightInnerContainerMobile
+  for (var i = 0; i < rightInnerContainer.length; i++) {
+    rightInnerContainer[i].classList.add("rightInnerContainerMobile");
+  }
 
-    // remove rightInnerContainer - USE REVERSE LOOP WHEN REMOVING CLASS!!
-    for (var i = rightInnerContainer.length - 1; i >= 0; i--) {
-        rightInnerContainer[i].classList.remove("rightInnerContainer")
-    }
+  // remove rightInnerContainer - USE REVERSE LOOP WHEN REMOVING CLASS!!
+  for (var i = rightInnerContainer.length - 1; i >= 0; i--) {
+    rightInnerContainer[i].classList.remove("rightInnerContainer");
+  }
 }
 
 function removeMobile(container, leftInnerContainer, rightInnerContainer, hr) {
-    // add and then remove
-    // add container
-    for (var i = 0; i < container.length; i++) {
-        container[i].classList.add("container")
-    }
+  // add and then remove
+  // add container
+  for (var i = 0; i < container.length; i++) {
+    container[i].classList.add("container");
+  }
 
-    // remove containerMobile - USE REVERSE LOOP WHEN REMOVING CLASS!!
-    for (var i = container.length - 1; i >= 0; i--) {
-        container[i].classList.remove("containerMobile")
-    }
+  // remove containerMobile - USE REVERSE LOOP WHEN REMOVING CLASS!!
+  for (var i = container.length - 1; i >= 0; i--) {
+    container[i].classList.remove("containerMobile");
+  }
 
-    // debugging - container.length is now empty
-    // for (var i = container.length-1; i >= 0; i--) {
-    //     console.log(container[i])
-    // }
+  // debugging - container.length is now empty
+  // for (var i = container.length-1; i >= 0; i--) {
+  //     console.log(container[i])
+  // }
 
-    // remove hrWidth
-    for (var i = hr.length - 1; i >= 0; i--) {
-        hr[i].classList.remove("hrWidth")
-    }
+  // remove hrWidth
+  for (var i = hr.length - 1; i >= 0; i--) {
+    hr[i].classList.remove("hrWidth");
+  }
 
-    // add leftInnerContainer
-    for (var i = 0; i < leftInnerContainer.length; i++) {
-        leftInnerContainer[i].classList.add("leftInnerContainer")
-    }
+  // add leftInnerContainer
+  for (var i = 0; i < leftInnerContainer.length; i++) {
+    leftInnerContainer[i].classList.add("leftInnerContainer");
+  }
 
-    // remove leftInnerContainerMobile - USE REVERSE LOOP WHEN REMOVING CLASS!!
-    for (var i = leftInnerContainer.length - 1; i >= 0; i--) {
-        leftInnerContainer[i].classList.remove("leftInnerContainerMobile")
-    }
+  // remove leftInnerContainerMobile - USE REVERSE LOOP WHEN REMOVING CLASS!!
+  for (var i = leftInnerContainer.length - 1; i >= 0; i--) {
+    leftInnerContainer[i].classList.remove("leftInnerContainerMobile");
+  }
 
-    // add rightInnerContainer
-    for (var i = 0; i < rightInnerContainer.length; i++) {
-        rightInnerContainer[i].classList.add("rightInnerContainer")
-    }
+  // add rightInnerContainer
+  for (var i = 0; i < rightInnerContainer.length; i++) {
+    rightInnerContainer[i].classList.add("rightInnerContainer");
+  }
 
-    // remove rightInnerContainerMobile - USE REVERSE LOOP WHEN REMOVING CLASS!!
-    for (var i = rightInnerContainer.length - 1; i >= 0; i--) {
-        rightInnerContainer[i].classList.remove("rightInnerContainerMobile")
-    }
+  // remove rightInnerContainerMobile - USE REVERSE LOOP WHEN REMOVING CLASS!!
+  for (var i = rightInnerContainer.length - 1; i >= 0; i--) {
+    rightInnerContainer[i].classList.remove("rightInnerContainerMobile");
+  }
 }
 
 // change layout upon window's innerWidth
-console.log(window.innerWidth)
+console.log(window.innerWidth);
 if (window.innerWidth < 575) {
-    var container = document.getElementsByClassName("container");
-    var leftInnerContainer = document.getElementsByClassName("leftInnerContainer");
-    var rightInnerContainer = document.getElementsByClassName("rightInnerContainer");
-    var hr = document.getElementsByTagName("hr")
+  var container = document.getElementsByClassName("container");
+  var leftInnerContainer = document.getElementsByClassName(
+    "leftInnerContainer"
+  );
+  var rightInnerContainer = document.getElementsByClassName(
+    "rightInnerContainer"
+  );
+  var hr = document.getElementsByTagName("hr");
 
-    addMobile(container, leftInnerContainer, rightInnerContainer, hr)
+  addMobile(container, leftInnerContainer, rightInnerContainer, hr);
 }
 
 // change layout upon resizing
-var onresize = function (e) {
-    width = e.target.outerWidth;
-    height = e.target.outerHeight;
+var onresize = function(e) {
+  width = e.target.outerWidth;
+  height = e.target.outerHeight;
 
-    console.log("resizing width=" + width)
-    if (width < 575) {
-        var container = document.getElementsByClassName("container");
-        var leftInnerContainer = document.getElementsByClassName("leftInnerContainer");
-        var rightInnerContainer = document.getElementsByClassName("rightInnerContainer");
-        var hr = document.getElementsByTagName("hr")
+  console.log("resizing width=" + width);
+  if (width < 575) {
+    var container = document.getElementsByClassName("container");
+    var leftInnerContainer = document.getElementsByClassName(
+      "leftInnerContainer"
+    );
+    var rightInnerContainer = document.getElementsByClassName(
+      "rightInnerContainer"
+    );
+    var hr = document.getElementsByTagName("hr");
 
-        addMobile(container, leftInnerContainer, rightInnerContainer, hr)
-    }
-    else if (width >= 575) {
-        container = document.getElementsByClassName("containerMobile");
-        leftInnerContainer = document.getElementsByClassName("leftInnerContainerMobile");
-        rightInnerContainer = document.getElementsByClassName("rightInnerContainerMobile");
-        hr = document.getElementsByTagName("hr")
+    addMobile(container, leftInnerContainer, rightInnerContainer, hr);
+  } else if (width >= 575) {
+    container = document.getElementsByClassName("containerMobile");
+    leftInnerContainer = document.getElementsByClassName(
+      "leftInnerContainerMobile"
+    );
+    rightInnerContainer = document.getElementsByClassName(
+      "rightInnerContainerMobile"
+    );
+    hr = document.getElementsByTagName("hr");
 
-        removeMobile(container, leftInnerContainer, rightInnerContainer, hr)
-    }
-}
+    removeMobile(container, leftInnerContainer, rightInnerContainer, hr);
+  }
+};
 window.addEventListener("resize", onresize);
-const hi = document.getElementById("hi");
-// animate when scrolling
-var dist = 100;
 
-window.onscroll = function () {
+// animate element when scroll down
+var containerAnimate = document.getElementsByClassName("animateScroll");
 
-    if (isScrolledIntoView(hi)) {
-        hi.style.background = "red"
+window.onscroll = function() {
+  for (var i = 0; i < containerAnimate.length; i++) {
+    if (isElementInViewport(containerAnimate[i])) {
+      if (containerAnimate[i].getElementsByClassName("leftInnerContainer")[0] != null) {
+        containerAnimate[i]
+          .getElementsByClassName("leftInnerContainer")[0]
+          .classList.add("fadeInFromLeftToRight");
+        containerAnimate[i]
+          .getElementsByClassName("rightInnerContainer")[0]
+          .classList.add("fadeInFromRightToLeft");
+      } else  {
+        containerAnimate[i]
+          .getElementsByClassName("leftInnerContainerMobile")[0]
+          .classList.add("fadeInFromLeftToRight");
+        containerAnimate[i]
+          .getElementsByClassName("rightInnerContainerMobile")[0]
+          .classList.add("fadeInFromRightToLeft");
+      }
     } else {
-        hi.style.background = ""
+      if (containerAnimate[i].getElementsByClassName("leftInnerContainer")[0] != null) {
+        containerAnimate[i]
+          .getElementsByClassName("leftInnerContainer")[0]
+          .classList.remove("fadeInFromLeftToRight");
+        containerAnimate[i]
+          .getElementsByClassName("rightInnerContainer")[0]
+          .classList.remove("fadeInFromRightToLeft");
+      } else {
+        containerAnimate[i]
+          .getElementsByClassName("leftInnerContainerMobile")[0]
+          .classList.remove("fadeInFromLeftToRight");
+        containerAnimate[i]
+          .getElementsByClassName("rightInnerContainerMobile")[0]
+          .classList.remove("fadeInFromRightToLeft");
+      }
     }
-
-    // for (var i = 0; i < animatedElements.length; i++) {
-    //     if (isScrolledIntoView(animatedElements[i])) {
-    //         console.log('in view');
-    //     } else {
-    //         console.log('out of view');
-    //     }
-    // }
+  }
 };
 
-function isScrolledIntoView(el) {
-    var rect = el.getBoundingClientRect();
-    var elemTop = rect.top;
-    var elemBottom = rect.bottom;
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  var elemTop = rect.top;
+  var elemBottom = rect.bottom;
 
-    // Only completely visible elements return true:
-    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-    // Partially visible elements return true:
-    //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
-    return isVisible;
+  console.log("elemTop=" + elemTop);
+  console.log("elemBottom=" + elemBottom);
+  console.log("window.innerHeight=" + window.innerHeight);
+
+  // Only completely visible elements return true:
+  // var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+
+  // elemBottom >= 0 is when the element is scrolled up past the screen
+  // elemTop < window.innerHeight is when the element is scrolled down and appears in the screen
+  // isVisible = elemTop + 50 < window.innerHeight && elemBottom >= 0;
+  isVisible = elemTop + 50 < window.innerHeight;
+  return isVisible;
 }
